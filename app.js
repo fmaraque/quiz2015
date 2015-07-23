db1887b26e46e298149a79e2818d4dff61372826
fmaraque <araquefran@gmail.com>
2015-07-23 17:27:58 +0200

    Ultima version

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('No Found');
     err.status = 404;
     next(err);
 });
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,26 +1,56 @@
 var models = require('../models/models.js');
 
-//GET /quizes/question
-exports.question = function (req, res){
- models.Quiz.findAll().success(function(quiz) {
-  res.render('quizes/question', {pregunta: quiz[0].pregunta});
- })
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+     function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{
+	next(new Error('No existe quizId=' + quizId));
+      }
+    }
+  ).catch(function(error){next(error);});
 };
 
-//GET /quizes/answer
-exports.answer = function (req, res){
- models.Quiz.findAll().success(function(quiz) {
 
-   if (req.query.respuesta === quiz[0].respuesta){
-	res.render ('quizes/answer', {respuesta: 'Correcto'});
-  } else {
-	res.render('quizes/answer', {respuesta: 'Incorrecto'});
-  }
- })
+//GET /quizes
+
+exports.index = function (req, res){
+ var busqueda = req.query.search;
+
+ if (busqueda === undefined){
+	busqueda ='%';
+ }else{
+        busqueda = '%' + busqueda.replace (/\s+/g,"%") + '%';
+ }
+
+ models.Quiz.findAll({ where: ["pregunta like ?", busqueda]}).then(function (quizes){
+           res.render('quizes/index', {quizes: quizes});
+        }
+      ).catch(function(error) { next(error);})
+};
+
+
+
+// Get /quizes/:id
+exports.show = function (req, res){
+	 res.render('quizes/show', {quiz: req.quiz});
+};
+
+// Get /quizes/:id/answer
+exports.answer = function (req, res){
+  var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto' ;
+     }
+     res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});     
 };
 
 //GET /author
 
 exports.author = function (req, res){
-	res.render ('author');
+        res.render ('author');
 };
+
